1db74235d3d6684ec2cc3c283749b18090f924f1
Andrew McAleavey <=>
2022-06-15 16:18:53 +0200

    simple RCI calculator working

--- R/RCIcalculators.R
+++ R/RCIcalculators.R
@@ -5,14 +5,10 @@
 #' Using a number of possible input combinations, most commonly time 1, 
 #' time 2, reliability, and SD value
 #'
-#' @param t1 Numeric.  Value or vector of values at time 1.  
-#' @param t2 Numeric.  Value or vector of values at time 2.  
 #' @param rxx Numeric.  Reliability coefficient to use in calculation
-#' @param diff Numeric.  Difference score. Calculated from \code{t1} and \code{t2} if not provided.
 #' @param sdiff Numeric.  Standard error of the difference score. Calculated if not provided.
 #' @param sem Numeric. Standard error of measurement. Calculated if not provided.
 #' @param sd1 Numeric. Standard deviation 1. Should be a group representative of the target group. Calculated if not provided. 
-#' @param sd2 Numeric. Standard deviation 2. Not required for J&T. 
 #' @param prob Numeric 0-1. Defaults to .975 to provide 95% two-sided confidence.  
 #' @param verbose Logical. If TRUE, will display additional information about the calculation.
 #'
@@ -20,16 +16,12 @@
 #' @export
 #'
 #' @examples RCI1 <- rci_calc_jt(42, 30, .8, 15)
-rci_calc_jt <- function(t1, 
-                     t2, 
-                     rxx = NULL, 
-                     sd1 = NULL, 
-                     diff = NULL, 
-                     sdiff = NULL, 
-                     sem = NULL, 
-                     sd2 = NULL,
-                     prob = .975,
-                     verbose = FALSE){
+rci_calc_jt <- function(rxx = NULL, 
+                        sd1 = NULL, 
+                        sdiff = NULL, 
+                        sem = NULL, 
+                        prob = .975,
+                        verbose = FALSE){
   # check for the correct type of data presented
   # if there is t1, there needs to be t2 and can set diff
   # if rxx is null, can't do anything
@@ -38,25 +30,10 @@ rci_calc_jt <- function(t1,
   }
   # if there is no sd1 provided, need to compute it and give warning
   if(is.null(sd1) & is.null(sem) & is.null(sdiff)){
-    sd1 <- sd(t1)
-    if(is.na(sd1)){
-      simpleError("No SD provided and cannot be inferred.")
-    } else if(!is.null(sd1) & is.null(sem) & is.null(sdiff)){
-    warning(paste("No SD provided, calculating from t1 values\n", 
-                  "The computed SD is", sd1))
-    }
-  }
+    simpleError("No SD provided and cannot be inferred.")
+  } 
   # now should have sd1
-  # if there is diff, don't need t1 or t2
-  if(!is.null(diff)){
-    if(!is.null(t1) | !is.null(t2)){
-      warning("t1 and t2 provided but so was diff. Disregarding t1 and t2.")
-    }
-  }
-  if(is.null(diff) & !is.null(t1) & !is.null(t2)){
-    diff <- t2 - t1
-  }
-  # now should have diff.
+  
   # if don't have sem yet, compute it
   if(is.null(sem)){
     sem <- sd1 * sqrt(1 - rxx)
@@ -69,10 +46,11 @@ rci_calc_jt <- function(t1,
   RCI <- prob * sdiff
   RCI
 }
-# rci_calc_jt(15, 10)
-# rci_calc_jt()
-# rci_calc_jt(15, 10, .8)
-# rci_calc_jt(15, 10, .8, 10)
+rci_calc_jt(.8, 10)
+rci_calc_jt()
+rci_calc_jt(.8, 10, .2)
+rci_calc_jt(.8, 10, .2, .2)
+rci_calc_jt(c(15))
 
 # temporary storage: 
 # calc_rci() takes minimally two vectors of data and computes the RCI for them. 
